c1dfae5232807e25da701f764ed8676633778f4f
a-markosyan <araks.markosyan98@gmail.com>
2024-12-05 18:04:35 +0400

    created curved design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import ShapedDivLayer from "./components/shapedDiv/ShapedDivLayer";
+import FlipInvertBottom from "./components/flipInvertBottom/FlipInvertBottom";
+import NoFlipNoInvertTop from "./components/noFlipNoInvertTop/NoFlipNoInvertTop";
 
 function App() {
-
   return (
     <div>
-      <ShapedDivLayer height={200}>
-      </ShapedDivLayer>
+      <header style={{ background: "#07086c", height: "200px" }}>Header</header>
+      <NoFlipNoInvertTop />
+      <section style={{ height: "300px" }}>mama mia</section>
+      <FlipInvertBottom />
+      <section
+        style={{
+          background: "#07086c",
+          height: "300px",
+          outline: "2px solid #07086c",
+        }}
+      >
+        mama mia
+      </section>
+      <NoFlipNoInvertTop />
+      <section style={{ height: "300px" }}>mama mia</section>
+      <FlipInvertBottom />
+      <section
+        style={{
+          background: "#07086c",
+          height: "300px",
+          outline: "2px solid #07086c",
+        }}
+      >
+        mama mia
+      </section>
+      <footer style={{ background: "#333333", height: "50px" }}>footer</footer>
     </div>
   );
 }
